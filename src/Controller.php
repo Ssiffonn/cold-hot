98eab536c10116e13eed5196014f9a066f5c270d
denis_razumov <wubmail@mail.ru>
2021-10-14 17:58:14 +0300

    Added SQLite3

--- src/Controller.php
+++ src/Controller.php
@@ -5,18 +5,16 @@ namespace ssiffonn\cold_hot\Controller;
 use SQLite3;
 
 use function ssiffonn\cold_hot\View\showGame;
-// use function ssiffonn\cold_hot\View\showList;
-use function ssiffonn\cold_hot\View\showReplay;
 use function ssiffonn\cold_hot\View\help;
 
-function key($key)
+function key($key, $id)
 {
     if ($key == "--new" || $key == "-n") {
         startGame();
     } elseif ($key == "--list" || $key == "-l") {
         showList();
     } elseif ($key == "--replay" || $key == "-r") {
-        showReplay();
+        showReplay($id);
     } elseif ($key == "--help" || $key == "-h") {
         help();
     } else {
@@ -27,19 +25,24 @@ function key($key)
 
 function coldHot($numberArray, $currentNumber)
 {
+    $result = "Исходы:";
     for ($i = 0; $i < 3; $i++) {
         if ($numberArray[$i] == $currentNumber[$i]) {
+            $result .= " Горячо!;";
             echo "Горячо!\n";
         } elseif (
             $numberArray[$i] == $currentNumber[0] ||
             $numberArray[$i] == $currentNumber[1] ||
             $numberArray[$i] == $currentNumber[2]
         ) {
+            $result .= " Тепло!;";
             echo "Тепло!\n";
         } else {
+            $result .= " Холодно!;";
             echo "Холодно!\n";
         }
     }
+    return $result;
 }
 
 function restart()
@@ -54,31 +57,11 @@ function restart()
 
 function startGame()
 {
-    $db = openDB();
-
-    date_default_timezone_set("Europe/Moscow");
-    $gameData = date("d") . "." . date("m") . "." . date("Y");
-    $gameTime = date("H") . ":" . date("i") . ":" . date("s");
-    $playerName = getenv("username");
-
-
     showGame();
     $number = 0;
     $currentNumber = random_int(100, 999);
-
-    $db->exec("INSERT INTO games (
-        gameDate, 
-        gameTime,
-        playerName,
-        secretNumber,
-        gameResult
-        ) VALUES (
-        '$gameData', 
-        '$gameTime',
-        '$playerName',
-        '$currentNumber',
-        'Не закончено'
-        )");
+    $db = insertDB($currentNumber);
+    $turn = 0;
 
     $currentNumber = str_split($currentNumber);
     $id = $db->querySingle("SELECT gameId FROM games ORDER BY gameId DESC LIMIT 1");
@@ -94,9 +77,16 @@ function startGame()
                     echo "Вы выиграли!\n";
                     $result = "Победа";
                     updateDB($id, $result);
+                    $turn++;
+                    $turnRes = coldHot($numberArray, $currentNumber);
+                    $turnResult = $turn . " | " . $number . " | " . $turnRes;
+                    insertReplay($id, $turnResult);
                     restart();
                 } else {
-                    coldHot($numberArray, $currentNumber);
+                    $turn++;
+                    $turnRes = coldHot($numberArray, $currentNumber);
+                    $turnResult = $turn . " | " . $number . " | " . $turnRes;
+                    insertReplay($id, $turnResult);
                 }
             }
         } else {
@@ -138,6 +128,32 @@ function createDB()
     return $db;
 }
 
+function insertDB($currentNumber)
+{
+    $db = openDB();
+
+    date_default_timezone_set("Europe/Moscow");
+    $gameData = date("d") . "." . date("m") . "." . date("Y");
+    $gameTime = date("H") . ":" . date("i") . ":" . date("s");
+    $playerName = getenv("username");
+
+    $db->exec("INSERT INTO games (
+        gameDate, 
+        gameTime,
+        playerName,
+        secretNumber,
+        gameResult
+        ) VALUES (
+        '$gameData', 
+        '$gameTime',
+        '$playerName',
+        '$currentNumber',
+        'Не закончено'
+        )");
+
+    return $db;
+}
+
 function updateDB($id, $result)
 {
     $db = openDB();
@@ -149,9 +165,42 @@ function updateDB($id, $result)
 function showList()
 {
     $db = openDB();
+    $query = $db->query('SELECT Count(*) FROM games');
+    $DBcheck = $query->fetchArray();
     $query = $db->query('SELECT * FROM games');
-    while ($row = $query->fetchArray()) {
-        \cli\line("ID $row[0])\n Дата: $row[1]\n Время: $row[2]\n Имя: $row[3]\n Загаданное число: $row[4]\n
-        Результат: $row[5]");
+    if ($DBcheck[0] != 0) {
+        while ($row = $query->fetchArray()) {
+            \cli\line("ID $row[0])\n Дата: $row[1]\n Время: $row[2] 
+ Имя: $row[3]\n Загаданное число: $row[4]\n Результат: $row[5]");
+        }
+    } else {
+        \cli\line("База данных пуста.");
+    }
+}
+
+function insertReplay($id, $turnResult)
+{
+    $db = openDB();
+    $db -> exec("INSERT INTO info (
+    gameID,
+    gameResult
+    ) VALUES (
+    '$id',
+    '$turnResult')");
+}
+
+function showReplay($id)
+{
+    $db = openDB();
+    $query = $db->query("SELECT Count(*) FROM info WHERE gameID = '$id'");
+    $DBcheck = $query->fetchArray();
+    if ($DBcheck[0] != 0) {
+        \cli\line("Повтор игры с id = " . $id);
+        $query = $db->query("SELECT gameResult FROM info WHERE gameID = '$id'");
+        while ($row = $query->fetchArray()) {
+            \cli\line("$row[0]");
+        }
+    } else {
+        \cli\line("База данных пуста, либо не правильный id игры.");
     }
 }
